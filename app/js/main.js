a915c0effd24be954a1b43cc16d1fd7175fade9c
Maxim <mvr1976@gmail.com>
2016-06-26 13:26:57 +0300

    commit 26/06

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+//flipover welcome
+$('.welcome_login').on('click', function(){
+		console.log('jquery is working');
+		$('.avatar').toggleClass('flipover');		
+		$('.log__in__welcome').toggleClass('flipover_hidden');
+		});
+
+//flipover welcome
